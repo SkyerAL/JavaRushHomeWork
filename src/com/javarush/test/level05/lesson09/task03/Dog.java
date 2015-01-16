6294e235bc76dcf7ad13b9f387ce08d70bb2683d
Алексей Дуюнов <duyunovas@gmail.com>
2015-01-16 18:49:10 +0300

    test

---
+++ src/com/javarush/test/level05/lesson09/task03/Dog.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level05.lesson09.task03;
+
+/* Создать класс Dog
+Создать класс Dog (собака) с тремя конструкторами:
+- Имя
+- Имя, рост
+- Имя, рост, цвет
+*/
+
+public class Dog
+{
+    private String name, color;
+    private int rost;
+
+    public Dog(String name)
+    {
+        this.name = name;
+    }
+
+    public Dog(String name, int rost)
+    {
+        this.name = name;
+        this.rost = rost;
+    }
+
+    public Dog(String name, String color, int rost)
+    {
+        this.name = name;
+        this.color = color;
+        this.rost = rost;
+    }
+}
