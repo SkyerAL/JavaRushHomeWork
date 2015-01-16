cf46d0d1cb07c655eb755b4e598805b116591c36
Алексей Дуюнов <duyunovas@gmail.com>
2015-01-16 15:54:24 +0300

    test l7 t3

--- src/com/javarush/test/level05/lesson07/task03/Dog.java
+++ src/com/javarush/test/level05/lesson07/task03/Dog.java
@@ -9,6 +9,25 @@ package com.javarush.test.level05.lesson07.task03;
 
 public class Dog
 {
-    //Напишите тут ваш код
+
+    private String name, color;
+    private int rost;
+
+    public void init(String name){
+        this.name = name;
+    }
+
+    public void init(String name, int rost){
+        this.name = name;
+        this.rost = rost;
+    }
+
+    public void init(String name, int rost, String color){
+        this.name = name;
+        this.rost = rost;
+        this.color = color;
+    }
+
+
 
 }
