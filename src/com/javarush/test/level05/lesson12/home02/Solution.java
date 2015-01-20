0b843f39409a0e31ea8c1ce0fd47d2140aa6a5e1
Алексей Дуюнов <duyunovas@gmail.com>
2015-01-20 12:45:50 +0300

    test lev 5 les12 h1

--- src/com/javarush/test/level05/lesson12/home02/Solution.java
+++ src/com/javarush/test/level05/lesson12/home02/Solution.java
@@ -13,9 +13,51 @@ public class Solution
     public static void main(String[] args)
     {
         // Создай по два объекта каждого класса тут
+        Man man1 = new Man("Ivan", "Decabristov", 34);
+        Man man2 = new Man("Petr", "Volhovka", 21);
 
+        Woman woman1 = new Woman("Luda", "red place", 18);
+        Woman woman2 = new Woman("Katya", "winter palace", 28);
         // Выведи их на экран тут
+
+        System.out.println(man1);
+        System.out.println(man2);
+        System.out.println(woman1);
+        System.out.println(woman2);
     }
 
     // Напиши тут свои классы
+    public static class Man{
+        private  String name, address;
+        private int age;
+
+        public Man(String name, String address, int age)
+        {
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+
+        public String toString(){
+            return name + " " + age + " " + address;
+        }
+    }
+
+    public static class Woman{
+        private  String name, address;
+        private int age;
+
+        public Woman(String name, String address, int age)
+        {
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+
+        public String toString(){
+            return name + " " + age + " " + address;
+        }
+
+
+    }
 }
