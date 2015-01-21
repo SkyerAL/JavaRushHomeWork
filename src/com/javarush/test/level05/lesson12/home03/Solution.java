fadbe95918816a7cad0344fd205f8077d16c0780
Алексей Дуюнов <duyunovas@gmail.com>
2015-01-21 13:13:35 +0300

    test lev 5 les12 h3

--- src/com/javarush/test/level05/lesson12/home03/Solution.java
+++ src/com/javarush/test/level05/lesson12/home03/Solution.java
@@ -1,7 +1,8 @@
 package com.javarush.test.level05.lesson12.home03;
 
 /* Создай классы Dog, Cat, Mouse
-Создай классы Dog, Cat, Mouse. Добавь по три поля в каждый класс, на твой выбор. Создай объекты для героев мультика Том и Джерри. Так много, как только вспомнишь.
+Создай классы Dog, Cat, Mouse. Добавь по три поля в каждый класс, на твой выбор.
+Создай объекты для героев мультика Том и Джерри. Так много, как только вспомнишь.
 Пример:
 Mouse jerryMouse = new Mouse(“Jerry”, 12 , 5), где 12 - высота в см, 5 - длина хвоста в см.
 */
@@ -11,6 +12,8 @@ public class Solution
     public static void main(String[] args)
     {
         Mouse jerryMouse = new Mouse("Jerry", 12 , 5);
+        Cat tomCat  = new Cat("Tom", 20, 123);
+        Dog bobDog  = new Dog("bob", 23, 222);
 
         //Напишите тут ваш код
     }
@@ -29,6 +32,30 @@ public class Solution
         }
     }
 
-    //Напишите тут ваши классы
+    public static class Dog
+    {
+        String name;
+        int height;
+        int tail;
 
+        public Dog(String name, int height, int tail)
+        {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Cat{
+        String name;
+        int height;
+        int tail;
+
+        public Cat(String name, int height, int tail)
+        {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
 }
