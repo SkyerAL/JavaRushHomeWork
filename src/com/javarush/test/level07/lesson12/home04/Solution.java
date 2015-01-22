c6d422e366da78a0fa520d3d7634223137f025e8
Алексей Дуюнов <duyunovas@gmail.com>
2015-01-22 18:44:37 +0300

    test level07 lesson12 task 04

---
+++ src/com/javarush/test/level07/lesson12/home04/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level07.lesson12.home04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* Вводить с клавиатуры строки, пока пользователь не введёт строку 'end'
+Создать список строк.
+Ввести строки с клавиатуры и добавить их в список.
+Вводить с клавиатуры строки, пока пользователь не введёт строку "end".  "end" не учитывать.
+Вывести строки на экран, каждую с новой строки.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //Напишите тут ваш код
+        ArrayList<String> list = new ArrayList<String>();
+
+        while (true)
+        {
+            String st = reader.readLine();
+            if (st.equals("end")) break;
+
+            list.add(st);
+
+        }
+
+        for (String s :list)
+        {
+            System.out.println(s);
+        }
+
+    }
+}
